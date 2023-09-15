46351aa545ec2f1af7a88ba2410674cdbe263c7c
0Uriel-Rodriguez <you@example.com>
2023-09-15 16:53:26 -0600

    Red equivalente a la de Tarea 2 pero usando Keras

---
+++ Network3_a_Keras.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import RMSprop, SGD
+
+dataset=mnist.load_data()
+(x_train, y_train), (x_test, y_test) = dataset
+x_trainv = x_train.reshape(60000, 784)
+x_testv = x_test.reshape(10000, 784)
+x_trainv = x_trainv.astype('float32')
+x_testv = x_testv.astype('float32')
+x_trainv /= 255
+x_testv /= 255
+
+num_classes=10
+y_trainc = keras.utils.to_categorical(y_train, num_classes)
+y_testc = keras.utils.to_categorical(y_test, num_classes)
+model = Sequential()
+model.add(Dense(50, activation='sigmoid', input_shape=(784,)))
+model.add(Dense(num_classes, activation='softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+history = model.fit(x_trainv, y_trainc,
+                    batch_size=100,
+                    epochs=30,
+                    verbose=1,
+                    validation_data=(x_testv, y_testc))
+
+score = model.evaluate(x_testv, y_testc, verbose=1) 
+print(score)
+a=model.predict(x_testv) 
+print(a.shape)
+print(a[1])
+print("resultado correcto:")
+print(y_testc[1])
+
+
+# In[ ]:
+
+
+
+
